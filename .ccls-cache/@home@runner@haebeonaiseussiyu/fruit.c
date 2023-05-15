73c3132b5abfa568a95091911434e28964371fe9
yumin0520 <129143518+yumin0520@users.noreply.github.com>
2023-05-15 02:17:37 +0000

    switch문 추가

---
+++ .ccls-cache/@home@runner@haebeonaiseussiyu/fruit.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+// main(){
+ 
+//   int a;
+//   printf("1:사과 , 2:키위, 3: 복숭아, 4:수박, 0:모두 좋아하지 않는다\n");
+//   printf("자신이 좋아하는 과일의 번호를 입력해주세요\n");
+//   scanf("%d", &a);
+  
+//   switch(a){
+//     case 1:
+//       printf("당신은 사과를 좋아하는군요!\n");
+//       break;
+//     case 2:
+//       printf("당신은 키위를 좋아하는군요!\n");
+//       break;
+//     case 3:
+//       printf("당신은 복숭아를 좋아하는군요!\n");
+//       break;
+//     case 4:
+//       printf("당신은 수박을 좋아하는군요!\n");
+//       break;
+//     default:
+//       printf("당신은 아마도 과일을 별로 좋아하지 않는 것 같군요!\n");
+//       break;
+//   }
+// }
+    
---
+++ .ccls-cache/@home@runner@haebeonaiseussiyu/mission_myscore.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+
+// main() {
+//   int score;
+
+//   printf("당신의 점수를 입력하세요: ");
+//   scanf("%d" , &score);
+
+//   if (score >= 90)
+//       printf("A 학점\n");  
+//   else if (score >=80)
+//       printf("B 학점\n");
+//   else if (score >=70)
+//     printf("C 학점\n");
+//   else if (score >=60)
+//     printf("D 학점\n");
+//   else
+//     printf("F 학점\n");
+  
+// }
